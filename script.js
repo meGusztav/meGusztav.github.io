c4f79e4966103418ae9bfcdca7a5cc10cefeb21f
meGusztav <gusztavfrancia97@gmail.com>
2025-05-20 12:04:11 -0700

    fixing js issues

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 import { createRoot } from 'react-dom/client';
 //old code
-			window.onload = function() { document.body.classList.remove('is-preload'); }
-			window.ontouchmove = function() { return false; }
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="wrapper"></div>';
-
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+window.onload = function() { document.body.classList.remove('is-preload'); }
+window.ontouchmove = function() { return false; }
